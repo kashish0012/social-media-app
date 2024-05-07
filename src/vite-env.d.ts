7a6a7430953c42007de67baa8c857bad3ec083cf
Kashish Kumar Singh <kashishkumar0012@gmail.com>
2024-05-07 14:47:34 +0530

    first commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMeta {
+  env: {
+    VITE_APPWRITE_PROJECT_ID: string;
+    VITE_APPWRITE_URL: string;
+    VITE_APPWRITE_STORAGE_ID: string;
+    VITE_APPWRITE_DATABASE_ID: string;
+    VITE_APPWRITE_SAVES_COLLECTION_ID: string;
+    VITE_APPWRITE_USER_COLLECTION_ID: string;
+    VITE_APPWRITE_POST_COLLECTION_ID: string;
+  };
+}
