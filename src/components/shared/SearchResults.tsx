87fa1d250679f963f7255560bd9578bd7c1926e6
Kashish Kumar Singh <kashishkumar0012@gmail.com>
2024-05-07 15:09:42 +0530

    search result fix

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,24 +1,6 @@
-import { Models } from 'appwrite';
-import React from 'react'
-import Loader from './Loader';
-import GridPostList from './GridPostList';
-
-type SearchResultProps = {
-    isSearchFetching: boolean;
-    searchedPosts: Models.Document[];
-}
-
-const SearchResults = ({isSearchFetching, searchedPosts} : SearchResultProps) => {
-    if (isSearchFetching) return <Loader />
-
-    if(searchedPosts && searchedPosts.documents.length > 0) {
-        return (
-            <GridPostList posts={searchedPosts.documents}/>
-        )
-    }
-    return (
-    
-    <p className='text-light-4 mt-10 text-center w-full'>No results found</p>
+const SearchResults = () => {
+  return (
+    <div>SearchResults</div>
   )
 }
 
